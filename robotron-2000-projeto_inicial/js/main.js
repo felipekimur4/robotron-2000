d73c2ce32f4d75b1b97a3f53626bb9fa923d22b1
felipekimur4 <felipekimura83@gmail.com>
2023-02-04 11:05:59 -0300

    ]cria pasta js e arquivo main.js

---
+++ robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,14 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", function(){
+    console.log("Cliquei no robo")
+})
+
+function oi() {
+    console.log("oi")
+    console.log("bem vindo")
+}
+
+oi(){
+    
+}
